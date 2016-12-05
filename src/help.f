cc3e1645ecba2a671110833bb2406f7432218c11
Fill4 <filipe4r@gmail.com>
2016-12-05 01:46:32 +0000

    Update help.f

--- src/help.f
+++ src/help.f
@@ -8,6 +8,8 @@ write(*,*) '   -h, --help        ', 'Show this help message and exit'
 write(*,*) '   -v,--verbose      ', 'Prints information during code execution'
 write(*,*) '   -p,--plots        ', 'Shows plots at the end of execution with the results'
 write(*,*) ' '
+write(*,*) ' Extra parameters can be tweaked in the file options_file'
+write(*,*) ' '
 write(*,*) ' File freqs has the structure:'
 write(*,*) ' '
 write(*,*) ' file_with_frequencies'
@@ -21,6 +23,9 @@ write(*,*) '    0   10    2000.00    0.047'
 write(*,*) '    0   12    2235.12    0.053'
 write(*,*) '    1   10    2492.32    0.106'
 write(*,*) ' '
+write(*,*) ' For more information check the repository at: https://github.com/Fill4/sigs_diff'
+write(*,*) ' '
+
 
 stop
 
